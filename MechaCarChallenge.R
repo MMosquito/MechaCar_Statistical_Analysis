2c81ae9dbcd6d7b8d791e8294eaf1cad184570ea
MMosquito <melissa.mosquito@gmail.com>
2023-03-27 20:41:27 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 cat("\014")
 
+#Part1: Linear Regression to Predict MPG
+
 #3 use the library function to load the dplyr package
 library(dplyr)
 
@@ -12,6 +14,16 @@ mechacar_mpg <- read.csv("MechaCar_mpg.csv", check.names=F, stringsAsFactors = F
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)
 
-#6 Use summary functio, determine the p-value and the r-squared value for linear regression model.
+#6 Use summary function, determine the p-value and the r-squared value for linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)) 
 
+#Part 2: Create Visualizations for the Trip Analysis
+
+#1/2 Import Suspension_Coil csv
+coil <- read.csv("Suspension_Coil.csv", check.names=F, stringsAsFactors = F)
+
+#3 Create total_summary dataframe using summarize
+total_summary <- coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI)) 
+
+#4 Create lot_summary dataframe    
+lot_summary <- coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI))
